7131fec211796696938aa936737511aee8c3937e
Michael Rice <54076544+michaelri-sellen@users.noreply.github.com>
2021-07-27 23:05:51 -0700

    Chunk uploading working

--- Main.py
+++ Main.py
@@ -6,8 +6,8 @@ if len(sys.argv) <= 1:
     print("Please include a file name")
     quit()
 
-#Open connection up to Logjam attack due to limitations of the WS1 API.
-#TODO: REMOVE THE LINE BELOW WHEN THE API IS UPDATED TO REMOVE THIS VULNERABILITY!!!!
+# Open connection up to Logjam attack due to limitations of the WS1 API.
+# TODO: REMOVE THE LINE BELOW WHEN THE API IS UPDATED TO REMOVE THIS VULNERABILITY!!!!
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += 'DEFAULT:!DH'
 
 def GetToken():
@@ -55,7 +55,7 @@ def CallAPI(endpoint, data, validate = False):
     while validate and response.status_code != 200:
         print(f"Validation failed with validate: True and status_code: {response.status_code}. Reattempting validation...")
         response = __Call(endpoint, data)
-    
+
     return response
 
 def UploadAppChunks():
@@ -77,7 +77,7 @@ def UploadAppChunks():
                     "TransactionId": transaction_id,
                     "ChunkData": base64.b64encode(bytes[current_byte : current_byte + Config.chunk_size]).decode(),
                     "ChunkSequenceNumber": sequence_num,
-                    "TotalApplicationSize": len(bytes), # Should this be the total size in bytes or the total chunk count? The API doesn't seem to care, using either doesn't appear to make a difference.
+                    "TotalApplicationSize": len(bytes),
                     "ChunkSize": Config.chunk_size
                 }
                 data = json.dumps(data)
@@ -97,7 +97,7 @@ def UploadAppChunks():
                     "TransactionId": transaction_id,
                     "ChunkData": base64.b64encode(bytes[current_byte :]).decode(),
                     "ChunkSequenceNumber": sequence_num,
-                    "TotalApplicationSize": len(bytes), # Should this be the total size in bytes or the total chunk count? The API doesn't seem to care, using either doesn't appear to make a difference.
+                    "TotalApplicationSize": len(bytes),
                     "ChunkSize": tail_size
                 }
                 data = json.dumps(data)
@@ -106,12 +106,12 @@ def UploadAppChunks():
         else:
             file.seek(0)
             data = {
-                    "TransactionId": "",
-                    "ChunkData": base64.b64encode(bytes).decode(),
-                    "ChunkSequenceNumber": 1,
-                    "TotalApplicationSize": len(bytes), # Should this be the total size in bytes or the total chunk count? The API doesn't seem to care, using either doesn't appear to make a difference.
-                    "ChunkSize": len(bytes)
-                }
+                "TransactionId": "",
+                "ChunkData": base64.b64encode(bytes).decode(),
+                "ChunkSequenceNumber": 1,
+                "TotalApplicationSize": len(bytes),
+                "ChunkSize": len(bytes)
+            }
             data = json.dumps(data)
 
             result = CallAPI("/mam/apps/internal/uploadchunk", data, False)
@@ -128,11 +128,11 @@ def UploadAppBlob():
 
     return blob_id
 
-def CreateApp(app_id, blob = False):    
+def CreateApp(app_id, blob = False):
     data = {
         "ApplicationName": sys.argv[1],
         "AutoUpdateVersion": True,
-        "BlobId" if blob else "TranscationId": app_id,
+        "BlobId" if blob else "TransactionId": f"{app_id}",
         "DeviceType": "WinRT",
         "SupportedModels": {
             "Model": [
@@ -144,28 +144,39 @@ def CreateApp(app_id, blob = False):
         },
         "PushMode": "OnDemand",
         "Description": "Text value",
-        "EnableProvisioning": False,
+        "EnableProvisioning": True,
         "UploadViaLink": False,
-        "FileName": sys.argv[1],
-        "IsDependencyFile": False,
         "LocationGroupId": 14522,
+        "AppVersion": "6.0.708",
+        "BuildVersion": "Text value",
+        "FileName": sys.argv[1],
+        "SupportedProcessorArchitecture": "x64",
+        "MsiDeploymentParamModel": {
+            "CommandLineArguments": "Text value",
+            "InstallTimeoutInMinutes": 2,
+            "RetryCount": 3,
+            "RetryIntervalInMinutes": 4,
+            "InstallContext": "Unknown"
+        },
+        "IsDependencyFile": False,
         "DeploymentOptions": {
             "WhenToInstall": {
                 "DataContingencies": [],
-                "DiskSpaceRequiredInKb": 10,
-                "DevicePowerRequired": 20,
-                "RamRequiredInMb": 30
+                "DiskSpaceRequiredInKb": 1,
+                "DevicePowerRequired": 2,
+                "RamRequiredInMb": 3
             },
             "HowToInstall": {
                 "InstallContext": "Device",
-                "InstallCommand": "echo test",
+                "InstallCommand": "Text value",
                 "AdminPrivileges": True,
                 "DeviceRestart": "DoNotRestart",
-                "RetryCount": 0,
-                "RetryIntervalInMinutes": 0,
-                "InstallTimeoutInMinutes": 15,
-                "InstallerRebootExitCode": "1641",
-                "InstallerSuccessExitCode": "0"
+                "RetryCount": 5,
+                "RetryIntervalInMinutes": 6,
+                "InstallTimeoutInMinutes": 7,
+                "InstallerRebootExitCode": "1999",
+                "InstallerSuccessExitCode": "0",
+                "RestartDeadlineInDays": 10
             },
             "WhenToCallInstallComplete": {
                 "UseAdditionalCriteria": True,
@@ -174,33 +185,50 @@ def CreateApp(app_id, blob = False):
                     {
                         "CriteriaType": "FileExists",
                         "FileCriteria": {
-                                "Path": "C:\\Program Files\\test\\test",
-                                "VersionCondition": "Any",
-                                "MajorVersion": 0,
-                                "MinorVersion": 0,
-                                "RevisionNumber": 0,
-                                "BuildNumber": 0,
-                                "ModifiedOn": "12-12-1999 12:00"
+                            "Path": "C:\\Program Files\\test\\test",
+                            "VersionCondition": "Any",
+                            "MajorVersion": 0,
+                            "MinorVersion": 0,
+                            "RevisionNumber": 0,
+                            "BuildNumber": 0,
+                            "ModifiedOn": "12-12-1999 12:00"
                         },
                         "LogicalCondition": "End"
                     }
                 ],
-            }
-        },
-        "FilesOptions": {
-            "AppTransformsList": [],
-            "AppPatchesList": [],
-            "ApplicationUnInstallProcess": {
-                "UseCustomScript": False,
                 "CustomScript": {
-                    "CustomScriptType": "Input",
-                    "UninstallCommand": "msiexec /x \"test\" /passive",
-                    "UninstallScriptBlobId": 0,
-                    "UseCustomScript": "true"
+                    "ScriptType": "Unknown",
+                    "CommandToRunTheScript": "Text value",
+                    "CustomScriptFileBlodId": 3,
+                    "SuccessExitCode": 1
                 }
             }
         },
-        "SupportedProcessorArchitecture": "x64"
+        "FilesOptions": {
+            "AppDependenciesList": [],
+            "AppTransformsList": [],
+            "AppPatchesList": [],
+            "ApplicationUnInstallProcess": {
+                "UseCustomScript": True,
+                "CustomScript": {
+                    "CustomScriptType": "Input",
+                    "UninstallCommand": "Text value",
+                    "UninstallScriptBlobId": 3
+                }
+            }
+        },
+        "CarryOverAssignments": True,
+        "ApplicationSource": 0,
+        "Criticality": 0,
+        "CategoryList": {
+            "Category": [
+                {
+                    "CategoryId": 1,
+                    "Name": "Education"
+                }
+            ]
+        },
+        "EARAppUpdateMode": 0
     }
 
     data = json.dumps(data)
@@ -210,7 +238,7 @@ def CreateApp(app_id, blob = False):
 
 
 # Set run to "blob" to upload file as Blob data or "chunk" to upload in Chunks
-run = "blob"
+run = "chunk"
 
 run_blob = run == "blob"
 result = CreateApp(UploadAppBlob() if run_blob else UploadAppChunks(), run_blob)
